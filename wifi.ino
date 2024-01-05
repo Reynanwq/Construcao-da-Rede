ca0095f65c41649f94a4cac98ad2e0506695dc72
Reynan Paiva <reynanwq@gmail.com>
2024-01-05 17:13:30 -0300

    adding ino files

---
+++ wifi.ino
@@ -0,0 +1,31 @@
+#include <WiFi.h>
+ 
+const char* ssid = "Reynan";
+const char* password = "reynan987";
+
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  Serial.println();
+  Serial.print("Conectando-se a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi conectada.");
+  Serial.println("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+ 
+  server.begin();
+}
+ 
+void loop() {
+    
+}
